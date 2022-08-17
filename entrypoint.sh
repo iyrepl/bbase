15d7464d3aafa6fd14669553ab2607a76933f1db
iyrepl <111183314+iyrepl@users.noreply.github.com>
2022-08-17 21:07:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,9 @@ port=1080
 passwd=lockey
 path=lockey
 
-# Download panindex
-wget -q https://github.com/libsgh/PanIndex/releases/latest/download/PanIndex-linux-amd64.tar.gz -O panindex.tar.gz
-tar -zxvf panindex.tar.gz
-mv PanIndex-linux-amd64 /usr/local/bin/panindex
-rm -f panindex.tar.gz & rm -f LICENSE
-chmod +x /usr/local/bin/panindex
-
 ## Install brook
 wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
 
 ## Start service
-panindex &
-/usr/bin/tor &
 brook wsserver --listen :${port} --password ${passwd} --path ${path}
